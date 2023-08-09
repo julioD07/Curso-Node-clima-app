981a8003d3c4a18f0d93f54d030892349f2e30f8
Julio Donado SanJuan <jdonado@unibaq.unibol.com.co>
2023-08-09 11:40:04 -0500

    Modelo para controlar la aplicacion

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const { leerInput, inquireMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
+
+    const busquedas = new Busquedas()
 
     let opt 
 
     do {
 
         opt = await inquireMenu("Favor escoja una opcion")
-        console.log({opt})
-        
+        // console.log({opt})
+
         switch (opt) {
             case 1:
-                
+                // Mostrar mensaje
+                const lugar = await leerInput("Ciudad: ")
+                console.log(lugar)
+                // Buscar los lugares
+
+                // Seleccionar el lugar
+
+                //Clima
+
+                //Mostrar los resultados
+
+                console.log('\nInformacion de la ciudad\n'.green)
+                console.log('Ciudad: ')
+                console.log('Lat: ')
+                console.log('Lng: ')
+                console.log('Temperatura: ')
+                console.log('Minima: ')
+                console.log('Maxima: ')
                 break;
             case 2:
                 
---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+
+
+
+class Busquedas {
+    historial = ['Barranquilla', 'Madrid', 'Bogota']
+
+    constructor() {
+        //TODO: leer DB si existe
+    }
+
+    async ciudad (lugar = '') {
+        // peticion HTTP
+        console.log(lugar)
+
+        return [] //Retornar los lugares
+    }
+}
+
+module.exports = Busquedas;
